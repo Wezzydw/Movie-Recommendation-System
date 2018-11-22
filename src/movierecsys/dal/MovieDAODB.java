b2b74f1e6ad3b3ce1537364fd62c363cce2b98c4
Wezzydw <44640169+Wezzydw@users.noreply.github.com>
2018-11-22 22:10:46 +0100

    MovieDAODB is done

--- src/movierecsys/dal/MovieDAODB.java
+++ src/movierecsys/dal/MovieDAODB.java
@@ -19,24 +19,72 @@ import movierecsys.be.Movie;
  *
  * @author Wezzy Laptop
  */
-public class MovieDAODB implements IMovieRepository {
+public class MovieDAODB implements IMovieRepository
+{
 
-    
     UandP up = new UandP();
+
     @Override
-    public Movie createMovie(int releaseYear, String title) throws IOException {
-        
-        
-        return null;
+    public Movie createMovie(int releaseYear, String title) throws IOException
+    {
+
+        int unUsedId = 0;
+        SQLServerDataSource ds = new SQLServerDataSource();
+        ds.setServerName("10.176.111.31");
+        ds.setDatabaseName("MRSDATABASE");
+        ds.setUser(up.getUser());
+        ds.setPassword(up.getPw());
+
+        try (Connection con = ds.getConnection())
+        {
+            Statement statement = con.createStatement();
+            int i = 0;
+            for (Movie m : getAllMovies())
+            {
+                if (m.getId() != i)
+                {
+                    unUsedId = i;
+                }
+                i++;
+            }
+            String sql = "INSERT INTO Movie (id, name, title) VALUES("
+                    + unUsedId + ","
+                    + releaseYear + ",'"
+                    + title.replace("'", "") + "');";
+            statement.execute(sql);
+
+        } catch (SQLException ex)
+        {
+            ex.printStackTrace();
+        }
+        Movie movie = new Movie(unUsedId, releaseYear, title);
+        return movie;
     }
 
     @Override
-    public void deleteMovie(Movie movie) throws IOException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public void deleteMovie(Movie movie) throws IOException
+    {
+        SQLServerDataSource ds = new SQLServerDataSource();
+        ds.setServerName("10.176.111.31");
+        ds.setDatabaseName("MRSDATABASE");
+        ds.setUser(up.getUser());
+        ds.setPassword(up.getPw());
+
+        try (Connection con = ds.getConnection())
+        {
+            Statement statement = con.createStatement();
+            String sql = "DELETE FROM Movie WHERE id = " + movie.getId() + ";";
+            statement.execute(sql);
+
+        } catch (SQLException ex)
+        {
+            ex.printStackTrace();
+        }
     }
 
     @Override
-    public List<Movie> getAllMovies() throws IOException {
+    public List<Movie> getAllMovies() throws IOException
+    {
 
         SQLServerDataSource ds = new SQLServerDataSource();
         ds.setServerName("10.176.111.31");
@@ -45,32 +93,74 @@ public class MovieDAODB implements IMovieRepository {
         ds.setPassword(up.getPw());
 
         List<Movie> movies = new ArrayList();
-        try (Connection con = ds.getConnection()) {
+        try (Connection con = ds.getConnection())
+        {
             Statement statement = con.createStatement();
             String sql = "SELECT * FROM MOVIE;";
             statement.execute(sql);
             ResultSet r = statement.getResultSet();
-            while(r.next())
+            while (r.next())
             {
-                Movie m = new Movie(r.getInt("id"),r.getInt("year"),r.getString("title"));
+                Movie m = new Movie(r.getInt("id"), r.getInt("year"), r.getString("title"));
                 movies.add(m);
             }
-                   
-            
-        } catch (SQLException ex) {
+
+        } catch (SQLException ex)
+        {
             ex.printStackTrace();
         }
         return movies;
     }
 
     @Override
-    public Movie getMovie(int id) throws IOException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public Movie getMovie(int id) throws IOException
+    {
+        Movie m = null;
+        SQLServerDataSource ds = new SQLServerDataSource();
+        ds.setServerName("10.176.111.31");
+        ds.setDatabaseName("MRSDATABASE");
+        ds.setUser(up.getUser());
+        ds.setPassword(up.getPw());
+
+        try (Connection con = ds.getConnection())
+        {
+            Statement statement = con.createStatement();
+           
+            String sql = "SELECT FROM Movie WHERE id = " + id + ";";
+            statement.execute(sql);
+            ResultSet r = statement.getResultSet();
+            m = new Movie(r.getInt("id"), r.getInt("year"), r.getString("title"));
+
+        } catch (SQLException ex)
+        {
+            ex.printStackTrace();
+        }
+        return m;
     }
 
     @Override
-    public void updateMovie(Movie movie) throws IOException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public void updateMovie(Movie movie) throws IOException
+    {
+        SQLServerDataSource ds = new SQLServerDataSource();
+        ds.setServerName("10.176.111.31");
+        ds.setDatabaseName("MRSDATABASE");
+        ds.setUser(up.getUser());
+        ds.setPassword(up.getPw());
+
+        try (Connection con = ds.getConnection())
+        {
+            Statement statement = con.createStatement();
+           
+            String sql = "UPDATE Movie SET year = "
+                    + movie.getYear() + ", title = '"
+                    + movie.getTitle().replace("'", "") + "' WHERE id = " + movie.getId() + ";";
+            statement.execute(sql);
+
+        } catch (SQLException ex)
+        {
+            ex.printStackTrace();
+        }
+
     }
 
 }
